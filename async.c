9cea9244629fe0b6ac2d9cfd795627e8dcd9fbae
Sudheer Hebbale <sudheer.hr@tekenlight.com>
2021-11-07 16:13:22 +0530

    fix

--- async.c
+++ async.c
@@ -199,7 +199,6 @@ redisAsyncContext *redisAsyncConnectWithOptions(const redisOptions *options) {
 redisAsyncContext *redisAsyncConnect(const char *ip, int port) {
     redisOptions options = {0};
     REDIS_OPTIONS_SET_TCP(&options, ip, port);
-	options.
     return redisAsyncConnectWithOptions(&options);
 }
 
